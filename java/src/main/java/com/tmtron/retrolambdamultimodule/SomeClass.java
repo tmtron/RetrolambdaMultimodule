c496bb5e04eea352d8d31451b2879a77ce42ef9e
TMTron <martin.trummer@tmtron.com>
2017-02-20 19:08:07 +0100

    added a dependency from the app to the java module

---
+++ java/src/main/java/com/tmtron/retrolambdamultimodule/SomeClass.java
@@ -0,0 +1,14 @@
+package com.tmtron.retrolambdamultimodule;
+
+public class SomeClass {
+
+    private final String val;
+
+    public SomeClass(String val) {
+        this.val = val;
+    }
+
+    public String getVal() {
+        return val;
+    }
+}
